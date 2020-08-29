ab0d44eb8ce61e4a30c4656b3e1f387464bfad65
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-08-29 16:58:45 -0500

    Se crean componentes loading, header, footer y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoadingComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
