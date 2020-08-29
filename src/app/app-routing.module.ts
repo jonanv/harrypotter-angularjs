945b85ff12fd95c5f62fd670d384100e9ef1469d
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-08-29 17:40:16 -0500

    Se crea rutas para el router-outlet y se crean componentes houses, wizards y spells

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { HousesComponent } from './components/houses/houses.component';
+import { WizardsComponent } from './components/wizards/wizards.component';
+import { SpellsComponent } from './components/spells/spells.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'houses', component: HousesComponent },
+  { path: 'wizards', component: WizardsComponent },
+  { path: 'spells', component: SpellsComponent },
+
+  { path: '**', pathMatch: 'full', redirectTo: 'houses' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Imports
+import { HttpClientModule } from "@angular/common/http";
+
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HousesComponent } from './components/houses/houses.component';
+import { WizardsComponent } from './components/wizards/wizards.component';
+import { SpellsComponent } from './components/spells/spells.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +21,15 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     HeaderComponent,
     LoadingComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    HousesComponent,
+    WizardsComponent,
+    SpellsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
