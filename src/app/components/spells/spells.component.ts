7b27503ed2a7a6615e93457705f815262e2d4cf3
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-08-30 10:36:21 -0500

    Se crea la estructura del componente spells

--- src/app/components/spells/spells.component.ts
+++ src/app/components/spells/spells.component.ts
@@ -25,7 +25,6 @@ export class SpellsComponent implements OnInit {
     this.harrypotterService.getSpells()
       .pipe(first())
       .subscribe((response: Spells[]) => {
-        console.log(response);
         this.spells = response;
       });
   }
